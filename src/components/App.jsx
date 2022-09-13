4b541d7d1841f508786750f140c4916cd11085bb
color <95304946+HermanMazur@users.noreply.github.com>
2022-09-13 14:08:37 +0300

    StatisticsJSX

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,16 @@ import user from 'data/user.json'
 export const App = () => {
     return (
         <div>
-<Profile
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-/>
+            <Profile
+                username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
+            <Statistics title="Upload stats" stats={data} />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
         </div>
     );
 };
